724f3cbf6c830c488bf0cce394eeb3be3ce82dd0
Mathias Picker <mattis123@live.no>
2023-09-01 14:12:37 +0200

    added repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { beforeNavigate } from '$app/navigation';
+
+    beforeNavigate((navigation) => {
+        navigation.cancel()
+    })
+</script>
+
+<a href="/other">Cancel navigation</a>
